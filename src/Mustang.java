a13bbcae2ab535d94a793905ee031cec20fd4c6e
HorseBarn <bjamil6532@bths.edu>
2018-11-16 15:04:16 -0500

    b

--- src/Mustang.java
+++ src/Mustang.java
@@ -1,3 +1,17 @@
-public class Mustang {
+public class Mustang implements Horse {
 
+    private int name;
+    private int weight;
+
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public int getWeight() {
+        return 0;
+    }
 }
+
